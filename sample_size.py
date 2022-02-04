be051db8a2a21fbed9bd71908c8573bf3f94313e
Henry Kpano <henry.kpano@meltwater.org>
2022-02-04 00:00:45 +0000

    update minimum detectable effect

--- sample_size.py
+++ sample_size.py
@@ -3,13 +3,25 @@ from statsmodels.stats.proportion import proportion_effectsize as es
 
 #define variable for the computation
 
-default_conversion = 0.50
+# current conversion rate or default conversion rate
+baseline_conversion = 0.20
+
+# minimum detectable effect - this refes to the sensitivity of the test.
+# In other words, it is the smallest relative change in baseline_conversion rate you are interested in detecting
+# This is also known as the lift rate between the current conversion rate and the targeted rate
+mde = 0.1
+
+
 alp = 0.05
-test_group_conversion = default_conversion + alp
+
+# alternative hypothesis expected conversion rate
+test_group_conversion = baseline_conversion + mde
+
+
 powerr = 0.8
 
 
-#ans = zt_ind_solve_power(effect_size=es(prop1=0.50, prop2=0.55), alpha=0.05, power=0.8, alternative="two-sided")
+#ans = zt_ind_solve_power(effect_size=es(prop1=0.20, prop2=0.6), alpha=0.05, power=0.8, alternative="two-sided")
 
-ans = zt_ind_solve_power(effect_size=es(prop1=default_conversion, prop2=test_group_conversion), alpha=alp, power=powerr, alternative="two-sided")
+ans = zt_ind_solve_power(effect_size=es(prop1=baseline_conversion, prop2=test_group_conversion), alpha=alp, power=powerr, alternative="two-sided")
 print(ans)
